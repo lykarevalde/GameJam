a26d76f4bf625ba384c02f8d7faa8b4fa4e3adcc
denden04 <tgcamalongay@up.edu.ph>
2026-01-28 14:33:50 +0800

    sad

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -49,12 +49,12 @@ func try_possess():
 	)
 
 	# scare humans
-	for pf in human_npcs_root.get_children():
-		if pf.get_child_count() == 0:
-			continue
-		var kid = pf.get_child(0)
-		if kid.global_position.distance_to(target_furniture.global_position) < 100:
-			kid.on_scared()
+	#for pf in human_npcs_root.get_children():
+		#if pf.get_child_count() == 0:
+			#continue
+		#var kid = pf.get_child(0)
+		#if kid.global_position.distance_to(target_furniture.global_position) < 100:
+			#kid.on_scared()
 
 func _on_furniture_finished(furniture):
 	if furniture != target_furniture:
